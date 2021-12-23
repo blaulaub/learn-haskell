08dc25884924fa9e531cacd09ef6ec286f66d1b8
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-23 22:28:51 +0100

    2-3-tree: empty node

--- 2-3-tree.hs
+++ 2-3-tree.hs
@@ -1,4 +1,7 @@
 -- record notation, with independent lists of keys and children
 data Node = Node { keys     :: [Int]
-                 , children :: [Maybe Node]
-                 }
+                 , children :: [Maybe Node]  -- for root: maybe none; for leaf: always none
+                 } deriving (Show)
+
+-- the empty node
+emptyNode = Node [] [Nothing]
