628604649d84b6e16ff79178122fd36ae736030a
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-23 23:57:30 +0100

    impl find in leaf

--- 2-3-tree.hs
+++ 2-3-tree.hs
@@ -6,12 +6,18 @@ data BTree = BTLeaf [Int]
 
 emptyBTree = BTLeaf []
 
+findInOrderedList :: [Int] -> Int -> Maybe Int
+findInOrderedList [] _ = Nothing
+findInOrderedList (x:xs) value | x < value  = findInOrderedList xs value
+                               | x == value = Just value
+                               | x > value  = Nothing
+
+findKey :: BTree -> Int -> Maybe Int
+findKey (BTLeaf xs) value = findInOrderedList xs value 
+
 splitOverflown :: BTree -> (BTree, Int, BTree)
 splitOverflown (BTLeaf xs) = (BTLeaf (take n xs), xs !! n, BTLeaf (drop (n+1) xs))
   where n = (length xs) `div` 2
 
-fixOverflow :: BTree -> BTree
-fixOverflow (BTLeaf xs) = BTLeaf xs  -- incorrect stub
-
 insert :: BTree -> Int -> BTree
-insert (BTLeaf xs) x = fixOverflow (BTLeaf (x:xs))  -- incorrect stub
+insert (BTLeaf xs) x = BTLeaf (x:xs)  -- incorrect stub
