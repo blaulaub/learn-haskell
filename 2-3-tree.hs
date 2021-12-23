fda627e4d33ccc1fc3c02797cd0e2b6ffa834c5a
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-23 23:43:50 +0100

    impl leaf split

--- 2-3-tree.hs
+++ 2-3-tree.hs
@@ -6,5 +6,12 @@ data BTree = BTLeaf [Int]
 
 emptyBTree = BTLeaf []
 
+splitOverflown :: BTree -> (BTree, Int, BTree)
+splitOverflown (BTLeaf xs) = (BTLeaf (take n xs), xs !! n, BTLeaf (drop (n+1) xs))
+  where n = (length xs) `div` 2
+
+fixOverflow :: BTree -> BTree
+fixOverflow (BTLeaf xs) = BTLeaf xs  -- incorrect stub
+
 insert :: BTree -> Int -> BTree
-insert (BTLeaf xs) x = BTLeaf (x:xs)  -- incorrect stub
+insert (BTLeaf xs) x = fixOverflow (BTLeaf (x:xs))  -- incorrect stub
