53160fb8b554ba3490ea2d39251a90ca785ad59f
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-24 00:01:00 +0100

    add sub-method for inserting in root (start case)

--- 2-3-tree.hs
+++ 2-3-tree.hs
@@ -19,5 +19,9 @@ splitOverflown :: BTree -> (BTree, Int, BTree)
 splitOverflown (BTLeaf xs) = (BTLeaf (take n xs), xs !! n, BTLeaf (drop (n+1) xs))
   where n = (length xs) `div` 2
 
+insertInRoot :: BTree -> Int -> BTree
+insertInRoot (BTLeaf xs) x = BTLeaf (x:xs)  -- incorrect stub
+
 insert :: BTree -> Int -> BTree
-insert (BTLeaf xs) x = BTLeaf (x:xs)  -- incorrect stub
+insert (BTLeaf xs) x = insertInRoot (BTLeaf xs) x
+
