20a51c274484a2991565c71abe05857efd2bd587
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-24 18:33:04 +0100

    add and use a 'listFrom' test helper

--- BTreeTests.hs
+++ BTreeTests.hs
@@ -2,6 +2,7 @@
 
 import Test.HUnit
 import qualified BTree
+import GHC.IO.Handle.Types (Handle__ (Handle__))
 
 --
 -- assertNotEqual not provided by HUnit
@@ -18,25 +19,28 @@ treeOf [] = BTree.emptyBTree
 treeOf (x : xs) = BTree.insert (treeOf xs) x
 
 --
-main = runTestTT (TestList [
+listFrom :: Ord key => BTree.BTree key -> [key]
+listFrom (BTree.BTLeaf xs) = xs
+listFrom (BTree.BTNode [] t) = listFrom t
+listFrom (BTree.BTNode ((t1, v1) : ts) tl) = listFrom t1 ++ [v1] ++ listFrom (BTree.BTNode ts tl)
 
+--
+main :: IO Counts
+main = runTestTT (TestList [
+  --
   TestLabel "constant for empty B-tree is well defined"
     (TestCase (assertEqual "emptyBTree is a leaf with no data" (BTree.BTLeaf []) (BTree.emptyBTree :: BTree.BTree Int))),
-
+  --
   TestLabel "constant for empty B-tree is well comparable"
     (TestCase (assertNotEqual "emptyBTree is not a leaf with one data item" (BTree.BTLeaf [42]) BTree.emptyBTree)),
-
+  --
   TestLabel "insert one element into empty tree gives one-leaf one-element tree"
     (TestCase (assertEqual "leaf consists of inserted key" (BTree.BTLeaf [13]) (BTree.insert BTree.emptyBTree 13))),
-
+  --
   TestLabel "insert lower element into singleton tree gives ordered one-leaf tree"
     (TestCase (assertEqual "leaf consists of inserted key" (BTree.BTLeaf [16, 27]) (BTree.insert (treeOf [27]) 16))),
-
-  TestLabel "insert higher element into singleton tree gives ordered one-leaf tree"
-    (TestCase (assertEqual "leaf consists of inserted key" (BTree.BTLeaf [1, 23]) (BTree.insert (treeOf [1]) 23))),
-
-  -- TODO this test assumes that a leaf can take up to four elements; rather compare flat list from tree
+  --
   TestLabel "building tree from a few elements gives ordered one-leaf tree"
-    (TestCase (assertEqual "leaf consists of inserted key" (BTree.BTLeaf [23, 34, 54, 76]) (treeOf [54, 23, 76, 34])))
+    (TestCase (assertEqual "leaf consists of inserted key" [23, 34, 54, 76] (listFrom (treeOf [54, 23, 76, 34]))))
 
   ])
