64807faf3c014d439746b8c728f0b21fcca1b19b
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-24 23:40:31 +0100

    revert to data structure (key and value)

--- BTree.hs
+++ BTree.hs
@@ -1,49 +1,9 @@
 module BTree where
 
---
-data BTree key
-  = BTLeaf [key]
-  | BTNode [(BTree key, key)] (BTree key)
+data BTree key value
+  = BTLeaf [(key, value)]
+  | BTNode [(BTree key value, (key, value))] (BTree key value)
   deriving (Eq, Show)
 
---
-emptyBTree :: Ord key => BTree key
+emptyBTree :: Ord key => BTree key value
 emptyBTree = BTLeaf []
---
-insertInOrderedList :: Ord key => [key] -> key -> [key]
-insertInOrderedList [] value = [value]
-insertInOrderedList (x : xs) value
-  | x < value = x : insertInOrderedList xs value
-  | x == value = value : xs
-  | x > value = value : x : xs
-insertInOrderedList (_ : _) _ = undefined
-
---
-data BTSplitResult key
-  = BTUnsplit (BTree key)
-  | BTSplit (BTree key, key, BTree key)
-
---
-splitIfOverflown :: Ord key => BTree key -> BTSplitResult key
-splitIfOverflown (BTLeaf xs)
-  | length xs > 2 = BTSplit (BTLeaf (take n xs), xs !! n, BTLeaf (drop (n + 1) xs))
-  | otherwise = BTUnsplit (BTLeaf xs)
-  where
-    n = length xs `div` 2
-splitIfOverflown (BTNode ps p) = BTUnsplit (BTNode ps p) -- incorrect stub
---
-
-rootFromSplit :: Ord key => BTSplitResult key -> BTree key
-rootFromSplit (BTUnsplit tree) = tree
-rootFromSplit (BTSplit (lower, key, upper)) = BTNode [(lower, key)] upper
-
---
-insertInRoot :: Ord key => BTree key -> key -> BTree key
-insertInRoot (BTLeaf xs) x = rootFromSplit (splitIfOverflown (BTLeaf (insertInOrderedList xs x))) -- incorrect stub
-insertInRoot (BTNode [] p) x = undefined
-insertInRoot (BTNode ps p) x = undefined
-
---
-insert :: Ord key => BTree key -> key -> BTree key
-insert (BTLeaf xs) x = insertInRoot (BTLeaf xs) x
-insert (BTNode ps p) x = insertInRoot (BTNode ps p) x
--- BTreeTests.hs
+++ BTreeTests.hs
@@ -12,38 +12,13 @@ assertNotEqual preface expected actual =
   assertBool msg (actual /= expected)
  where msg = (if null preface then "" else preface ++ "\n") ++
              "not expected: " ++ show expected ++ "\n but got: " ++ show actual
-
---
-treeOf :: Ord key => [key] -> BTree.BTree key
-treeOf [] = BTree.emptyBTree
-treeOf (x : xs) = BTree.insert (treeOf xs) x
-
---
-listFrom :: Ord key => BTree.BTree key -> [key]
-listFrom (BTree.BTLeaf xs) = xs
-listFrom (BTree.BTNode [] t) = listFrom t
-listFrom (BTree.BTNode ((t1, v1) : ts) tl) = listFrom t1 ++ [v1] ++ listFrom (BTree.BTNode ts tl)
-
 --
 main :: IO Counts
 main = runTestTT (TestList [
   --
-  TestLabel "constant for empty B-tree is well defined"
-    (TestCase (assertEqual "emptyBTree is a leaf with no data" (BTree.BTLeaf []) (BTree.emptyBTree :: BTree.BTree Int))),
-  --
-  TestLabel "constant for empty B-tree is well comparable"
-    (TestCase (assertNotEqual "emptyBTree is not a leaf with one data item" (BTree.BTLeaf [42]) BTree.emptyBTree)),
-  --
-  TestLabel "insert one element into empty tree gives one-leaf one-element tree"
-    (TestCase (assertEqual "leaf consists of inserted key" (BTree.BTLeaf [13]) (BTree.insert BTree.emptyBTree 13))),
-  --
-  TestLabel "insert lower element into singleton tree gives ordered one-leaf tree"
-    (TestCase (assertEqual "leaf consists of inserted key" (BTree.BTLeaf [16, 27]) (BTree.insert (treeOf [27]) 16))),
-  --
   TestLabel
-    "building tree from three elements gives ordered one-leaf tree"
-    (TestCase (assertEqual "leaf consists of inserted key" [19, 28, 64] (listFrom (treeOf [64, 19, 28]))))
-
+    "constant for empty B-tree is an empty leaf"
+    (TestCase (assertEqual "emptyBTree is a leaf with no data" (BTree.BTLeaf []) (BTree.emptyBTree :: BTree.BTree Int Int)))
   ])
 
 --
