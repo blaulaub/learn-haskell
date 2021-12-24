0c76c96639addf54b564afe74614672ae9650f60
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-24 14:25:54 +0100

    fix white spaces

--- 2-3-tree.hs
+++ 2-3-tree.hs
@@ -1,5 +1,6 @@
 
 -- B-Tree data structure
+
 data BTree = BTLeaf [Int]
            | BTNode [(BTree, Int)] BTree
            deriving (Show)
@@ -13,7 +14,7 @@ findInOrderedList (x:xs) value | x < value  = findInOrderedList xs value
                                | x > value  = Nothing
 
 findKey :: BTree -> Int -> Maybe Int
-findKey (BTLeaf xs) value = findInOrderedList xs value 
+findKey (BTLeaf xs) value = findInOrderedList xs value
 
 splitOverflown :: BTree -> (BTree, Int, BTree)
 splitOverflown (BTLeaf xs) = (BTLeaf (take n xs), xs !! n, BTLeaf (drop (n+1) xs))
@@ -24,4 +25,3 @@ insertInRoot (BTLeaf xs) x = BTLeaf (x:xs)  -- incorrect stub
 
 insert :: BTree -> Int -> BTree
 insert (BTLeaf xs) x = insertInRoot (BTLeaf xs) x
-
